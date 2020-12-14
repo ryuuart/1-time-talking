79933a936e4cf1127f6387112a1dd862ca76c443
Long Nguyen <18nguyenl@gmail.com>
2020-12-14 12:49:14 -0600

    update route for react static file sending

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ if(process.env.NODE_ENV === "production") {
     console.log("Serving server production");
     app.use(express.static(path.join(__dirname, 'build')));
 
-    app.get('/', function(req, res) {
+    app.get('*', function(req, res) {
         res.sendFile(path.join(__dirname, 'build', 'index.html'));
     })
 }
